8c53f6048cceb24afdc80f9803e7971ce11e0ed7
Monika Williams <monika.m.williams@gmail.com>
2017-12-22 11:22:12 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,40 @@
 class ProductsController < ApplicationController
+ before_action :set_product ,only: [:show,:edit,:update,:destroy]
+
   def index
     @products = Product.all
   end
 
-  def description
+  def description 
     product = Product.find(params[:id])
     render plain: product.description
   end
+
+  def show
+  end
+  
+  def new
+      @product = Product.new
+  end
+  
+  def create
+    @product = Product.create (product_params)
+    @product.save
+    redirect_to product_path (@product)
+  end
+  
+  def edit
+  end
+
+  def update
+    @product.update(product_params)
+    redirect_to product_path (@product)
+   end
+
+  private
+
+def set_product
+  @product = Product.find(params[:id])
+end
 
 end
